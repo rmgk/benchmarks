4f8d46310228e50bf07f8b98259c7b41dff4bef2
ragnar <ragnar@rmgk.de>
2015-03-27 17:17:25 +0100

    add size hint benchmark

---
+++ benchmarks/src/main/scala/de/rmgk/HashMapSize.scala
@@ -0,0 +1,33 @@
+package de.rmgk
+
+import java.util
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 4, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 300, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(2)
+class HashMapSize {
+	@Param(Array(/*"1", "10", "100", "1000",*/ "10000", "100000"))
+	var size: Int = _
+
+	lazy val keys = Range(0, size).map(_.toString)
+
+	@Benchmark
+	def insertWithoutSizeHint(): Boolean = {
+		val hm = new util.HashMap[String, Boolean]()
+		keys.foreach(k => hm.put(k, true))
+		hm.get("0")
+	}
+
+	@Benchmark
+	def insertWithSizeHint(): Boolean = {
+		val hm = new util.HashMap[String, Boolean](2 * size)
+		keys.foreach(k => hm.put(k, true))
+		hm.get("0")
+	}
+}
